dcf512f8ae27720f31af90ec3ab57af093d62048
dalva <dalvarechi@gmail.com>
2026-04-26 22:08:40 +0200

    Petición de datos y objtivos

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import streamlit as st
 import formulario
+
 # Título de la página
-st.header("Bien venido a Math Diet")
+st.header("Bienvenido a Math Diet")
 
 # Texto normal
 st.write("Esta página te va a ayudar a generar un menú personalizado en función de tus necesidades mediante Teoría de Grafos")
 
 # Un botón interactivo para probar
-if st.button("Empezar"):
-    formulario.mostrarFormulario()
- 
+datos = formulario.pedirDatosBiometricos()
+if datos:
+    st.write("Nombre:", datos["nombre"])
+    st.write("Peso:", datos["peso"], "kg")
+    st.write("Altura:", datos["altura"], "cm")
+    st.write("IMC:", datos["imc"])
+
+
